5e67da07b9064ebfa9ff7ddd050d32e898e3b4d9
vagsy <981604339@qq.com>
2017-12-20 18:58:01 +0800

    python_eg init commit

---
+++ pythondemo/1.py
@@ -0,0 +1,6 @@
+def fun(a, b=3):
+    return a + b
+
+
+print fun(1, 2)
+print fun(2)
---
+++ pythondemo/builtwitheg.py
@@ -0,0 +1,5 @@
+import builtwith;
+
+print builtwith.parse('http://example.webscraping.com');
+print builtwith.parse('http://www.windline.info');
+print builtwith.parse('http://bbit.top');

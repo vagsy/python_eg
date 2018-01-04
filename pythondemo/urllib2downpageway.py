1c5580d403046dfcf640e59ef1d2f1211818656b
vagsy <981604339@qq.com>
2018-01-04 11:47:12 +0800

    fix

--- pythondemo/urllib2downpageway.py
+++ pythondemo/urllib2downpageway.py
@@ -4,19 +4,19 @@ import urllib2, cookielib
 
 url = "http://www.baidu.com"
 
-print u'第一种方法'
+print '第一种方法'
 response1 = urllib2.urlopen(url)
 print response1.getcode()
 print len(response1.read())
 
-print u"第二种方法"
+print "第二种方法"
 request = urllib2.Request(url)
 request.add_header("user-agent", "Mozilla/5.0")
 response2 = urllib2.urlopen(request)
 print response2.getcode()
 print len(response2.read())
 
-print u"第三种方法"
+print "第三种方法"
 cj = cookielib.CookieJar()
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 urllib2.install_opener(opener)

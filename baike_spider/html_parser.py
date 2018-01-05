dd2eef6c75d673ce6f3f9282892414f90d7ae818
vagsy <981604339@qq.com>
2018-01-05 11:24:20 +0800

    from XXX rename bug fix

--- baike_spider/html_parser.py
+++ baike_spider/html_parser.py
@@ -7,7 +7,7 @@ class HtmlParser(object):
     
     def _get_new_urls(self, page_url, soup):
         new_urls = set()
-        links = soup.find_all('a',href = re.compile(r"/item/"))
+        links = soup.find_all('a',href = re.compile(r"/item/.*"))
         for link in links:
             new_url = link['href']
             new_full_url = urlparse.urljoin(page_url,new_url)

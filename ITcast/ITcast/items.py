a974bf3decbfee9c094be1bbb3c64c58b315f7f1
vagsy <981604339@qq.com>
2018-01-31 18:31:30 +0800

    fix

--- ITcast/ITcast/items.py
+++ ITcast/ITcast/items.py
@@ -10,5 +10,10 @@ import scrapy
 
 class ItcastItem(scrapy.Item):#类 ItcastItem  继承父类scrapy.Item
     # define the fields for your item here like:
+    # 老师姓名
     name = scrapy.Field() #创建一个item字段  提取的字段存储到字段里面去
+    # 老师职称
+    title = scrapy.Field()
+    # 老师信息
+    info = scrapy.Field()
     # pass
--- ITcast/ITcast/spiders/itcast.py
+++ ITcast/ITcast/spiders/itcast.py
@@ -1,13 +1,36 @@
 # -*- coding: utf-8 -*-
 import scrapy
+# 从...路径,引入...类
+from ITcast.items import ItcastItem
 
 
 class ItcastSpider(scrapy.Spider):
-    name = 'itcast' #爬虫名称
-    allowed_domains = ['http://www.itcast.cn'] #爬虫允许域的范围
-    start_urls = ['http://www.itcast.cn/'] #爬虫第一次启动时会从这个列表里开始抓取
+    # 爬虫名，启动爬虫时需要的参数*必需
+    name = "itcast"
+    # 爬取域范围，允许爬虫在这个域名下进行爬取（可选）
+    allowed_domains = ["http://www.itcast.cn"] #爬虫允许域的范围
+    # 起始url列表，爬虫执行后第一批请求，将从这个列表里获取
+    start_urls = ["http://www.itcast.cn/channel/teacher.shtml"] #爬虫第一次启动时会从这个列表里开始抓取
 
     # 处理响应文件
     def parse(self, response):
-        print (response.body)
+        # 节点列表
+        node_list = response.xpath("//div[@class='li_txt']")
+
+        #用来存储所有的item字段的
+        items = []
+        for node in node_list:
+            # 创建item字段对象，用来存储信息
+            item = ItcastItem()
+            # .extract() 将xpath对象转换为Unicode字符串
+            name = node.xpath("./h3/text()").extract()
+            title = node.xpath("./h4/text()").extract()
+            info = node.xpath("./p/text()").extract()
+
+            item["name"] = name[0]
+            item["title"] = title[0]
+            item["info"] = info[0]
+            items.append(item)
+
+        return items
         # pass

6c09cac6889433257235b2c6850bf2140fb38b0e
vagsy <981604339@qq.com>
2018-02-11 13:29:35 +0800

    fix

--- stack/stack/settings.py
+++ stack/stack/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'stack.spiders'
 #USER_AGENT = 'stack (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
